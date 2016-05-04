f46936b9954044edf02137a6196a038d418afd7d
IdarV <twintip_idar@hotmail.com>
2016-05-04 18:21:34 +0200

    added dynamic waiting

--- work.js
+++ work.js
@@ -1,24 +1,44 @@
-setTimeout(function(){
-  $('.aid-background-blur').addClass("aid-background").removeClass("aid-background-blur");
-  $('#aid-overlay').remove();
-  $('.aid-large-center').remove();
+window.onload = function () {
+    // Check that elements exists after page has loaded
+    if (document.getElementById("aid-overlay") != null && document.getElementsByClassName("am-article-summary") != null) {
+        waitForElement('.am-gridComp-body', function () {
+            $('.aid-background-blur').addClass("aid-background").removeClass("aid-background-blur");
+            $('#aid-overlay').remove();
+            $('.aid-large-center').remove();
 
-  var url = document.location.href;
-  var split = url.split('/');
-  var id = split[split.length - 1];
+            var url = document.location.href;
+            var split = url.split('/');
+            var id = split[split.length - 1];
 
-  $.get('http://bed.api.no/api/acpcomposer/v1.1/content/' + id)
-      .done(function (data) {
-        appendData(data);
-      })
-      .fail(function () {
-        console.log("Could not load API");
-      });
+            $.get('http://bed.api.no/api/acpcomposer/v1.1/content/' + id)
+                .done(function (data) {
+                    appendData(data);
+                })
+                .fail(function () {
+                    console.log("Could not load API");
+                });
+        });
+    }
 
-},3000);
+};
 
-function appendData(data){
-  $('<div class="am-article-body am-txt--body" itemprop="articleBody"></div>').insertAfter(".am-article-summary");
+function appendData(data) {
+    $('<div class="am-article-body am-txt--body" itemprop="articleBody"></div>').insertAfter(".am-article-summary");
+    $('.am-article-body').append(data.body);
+}
 
-  $('.am-article-body').append(data.body);
+var n = 0;
+function waitForElement(elementPath, callBack) {
+    window.setTimeout(function () {
+        n++;
+        if ($(elementPath).length) {
+            callBack(elementPath, $(elementPath));
+        } else {
+            if (n < 20) {
+                waitForElement(elementPath, callBack);
+            } else{
+                console.log("AntiPluss ventet for lenge på artikkelen.")
+            }
+        }
+    }, 500)
 }
